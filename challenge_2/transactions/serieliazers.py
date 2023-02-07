42342f6b038db470f1710e0c5c3eb1ab87c97943
Yuri Bentes <eypbentes@gmail.com>
2023-02-07 15:42:31 -0300

    Create:Serializers

---
+++ challenge_2/transactions/serieliazers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Transactions
+
+class TransactionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transactions
+        fields = '__all__'
